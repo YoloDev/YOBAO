fe7e98c2b8507f9d80a11ee44e63648ec54b0cae
Richard Bennett <richard@adaptpos.com>
2014-08-15 19:23:52 -0400

    moved things around to get app running again.

--- Source/Yobao/Yobao.cs
+++ Source/Yobao/Yobao.cs
@@ -47,17 +47,4 @@ namespace Yobao {
 			Configurations.Add(dataConfiguration);
 		}
 	}
-
-	public class DataConfiguration<T, TResult> where TResult : class {
-		public string Name { get; set; }
-
-		public bool VisibleInMenu { get; set; }
-
-		public Type ElementType { get; set; }
-		public Type DataProviderType { get; set; }
-
-		public T DataProvider { get; set; }
-		public IQueryable<TResult> Query { get; set; }
-
-	}
 }
--- Source/Yobao/YobaoNancyBootstrapper.cs
+++ Source/Yobao/YobaoNancyBootstrapper.cs
@@ -2,6 +2,10 @@
 	using Nancy;
 	using Nancy.TinyIoc;
 	public class YobaoNancyBootstrapper : DefaultNancyBootstrapper {
+		IDataSource _DataSource;
+		public YobaoNancyBootstrapper() : base() {
+			_DataSource = new Yobao<SampleDatabase>(new SampleDatabase());
+		}
 
 		//placeholder for application container setup (if needed, when needed)
 		protected override void ConfigureApplicationContainer(TinyIoCContainer container) {
@@ -11,6 +15,8 @@
 		//placeholder for request specific container setup (if needed, when needed)
 		protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context) {
 			base.ConfigureRequestContainer(container, context);
+
+			container.Register<IDataSource>(_DataSource);
 		}
 	}
 }
