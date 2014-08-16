2f5ad220f835222ff141bdd45d40f8cd7cfc5c33
Richard Bennett <richard@adaptpos.com>
2014-08-15 20:05:25 -0400

    moved modules into 'Core' package to support drop-in to a project.

---
+++ Source/Yobao.Core/YobaoStorageModule.cs
@@ -0,0 +1,24 @@
+﻿namespace Yobao {
+	using Nancy;
+	using Nancy.ModelBinding;
+
+	using System;
+	
+	public class YobaoStorageModule : NancyModule {
+		public YobaoStorageModule(IDataSource yobao, IDataPersistence dataPersistence) {
+			Post["/{type}/create"] = _ => {
+				var formType = yobao.ResolveType((string)_.type);
+				var formObj = Activator.CreateInstance(formType);
+				this.BindTo(formObj);
+				dataPersistence.Persist(formObj);
+				return Response.AsRedirect(string.Format("/{0}/list", (string)_.type));
+			};
+			Put["/{type}/edit/{id}"] = _ => {
+				var formObj = yobao.Load((string)_.type, (object)_.id);
+				this.BindTo(formObj);
+				dataPersistence.Persist(formObj);
+				return Response.AsRedirect(string.Format("/{0}/list", (string)_.type));
+			};
+		}
+	}
+}
